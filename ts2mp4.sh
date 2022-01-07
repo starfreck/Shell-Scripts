b06ea4bd220333e8a56187259e912772b6b86b44
StarFreck <vasuratanpara@gmail.com>
2022-01-06 19:20:02 -0500

    Add files via upload

---
+++ ts2mp4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+start=`date +%s`
+
+echo "Executing $0"
+echo "The process id is $$"
+echo "The file '$1' will be converted to '$2'"
+
+#ffmpeg -i $1 -c:v libx264 $2
+
+end=`date +%s`
+
+echo "Process finished"
+
+runtime=$((end-start))
+hours=$((runtime / 3600))
+minutes=$(( (runtime % 3600) / 60 ))
+seconds=$(( (runtime % 3600) % 60 ))
+
+echo "----------------------------------------------------------------------"
+echo "Runtime: $hours:$minutes:$seconds (hh:mm:ss)"
+echo "----------------------------------------------------------------------"
+
